702ac4c8e7fe5959e274edc641131ba260a75d41
github aggrmh <mhkawsdd1027@gmail.com>
2022-04-06 16:28:36 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    クラスベースのコンポーネント
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
+</div>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -3,4 +3,7 @@
 コンポーネントテスト2
 
 <x-tests.card title="タイトル2" content="コンテンツ" />
+<x-test-class-base classBaseMessage="メッセージです"/>
+<div class="mb-4"></div>
+<x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更しています" />
 </x-tests.app>
